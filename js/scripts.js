82907bb8c0c66cac4983f841f93ee9d2ebc7e90d
ancapuscasu <83923018+ancapuscasu@users.noreply.github.com>
2021-07-07 20:24:48 -0400

    started pokemon list and added 4 pokemon
    added keys: name, height (in inches), and types

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,26 @@
-alert('Hello World');
+let pokemonList = [];
+// height = inches
 
-let favoriteFood = 'cake';
-document.write(favoriteFood);
+pokemonList.poke1 = {
+	name: 'Bulbasaur',
+	height: 28,
+	types: ['grass', 'poison']
+}
+
+pokemonList.poke2 = {
+	name: 'Charizard',
+	height: 67,
+	types: ['fire', 'flying']
+}
+
+pokemonList.poke3 = {
+	name: 'Butterfree',
+	height: 43,
+	types: ['bug', 'flying']
+}
+
+pokemonList.poke4 = {
+	name: 'Dugtrio',
+	height: 28,
+	types: ['ground']
+}
