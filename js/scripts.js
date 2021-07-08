47a3ae0a5cd2d5f4a7f7cbfd4e06b0fa1fcad457
ancapuscasu <83923018+ancapuscasu@users.noreply.github.com>
2021-07-07 23:44:24 -0400

    corrected pokemonList objects

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,26 @@
-let pokemonList = [];
+let pokemonList = [
 // height = inches
-
-pokemonList.poke1 = {
+{
 	name: 'Bulbasaur',
 	height: 28,
 	types: ['grass', 'poison']
-}
+},
 
-pokemonList.poke2 = {
+{
 	name: 'Charizard',
 	height: 67,
 	types: ['fire', 'flying']
-}
+},
 
-pokemonList.poke3 = {
+{
 	name: 'Butterfree',
 	height: 43,
 	types: ['bug', 'flying']
-}
+},
 
-pokemonList.poke4 = {
+{
 	name: 'Dugtrio',
 	height: 28,
 	types: ['ground']
 }
+]
